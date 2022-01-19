d6e95a9314a99502692a14f66f732605eec14a27
Tomer L <tomer.lotan@sap.com>
2022-01-19 10:13:17 +0200

    add tests

--- apps/restful-queue/src/queue/queue.controller.spec.ts
+++ apps/restful-queue/src/queue/queue.controller.spec.ts
@@ -17,4 +17,38 @@ describe('QueueController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create new queue', () => {
+    const res = controller.create('test1');
+    expect(res.status).toEqual('ok');
+  });
+
+  it('should another new queue', () => {
+    const res = controller.create('test2');
+    expect(res.status).toEqual('ok');
+  });
+
+  it('should throw error 404 when create queue which is already exist', () => {
+    let res = controller.create('test2');
+    try {
+      res = controller.create('test2');
+    } catch (error) {
+      expect(error.status).toEqual(409);
+    }
+  });
+
+  it('should enqueue and dequeue by queue name', () => {
+    controller.create('test1');
+    controller.create('test2');
+    controller.enqueue('test1', 'item1');
+    controller.enqueue('test1', 'item2');
+    let test1Queue = controller.snapshot('test1');
+    const test2Queue = controller.snapshot('test2');
+    expect(test1Queue.length).toEqual(2);
+    expect(test2Queue.length).toEqual(0);
+    controller.dequeue('test1');
+    test1Queue = controller.snapshot('test1');
+    expect(test1Queue.length).toEqual(1);
+    expect(test2Queue.length).toEqual(0);
+  });
 });
--- apps/restful-queue/src/queue/queue.service.spec.ts
+++ apps/restful-queue/src/queue/queue.service.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Queue } from './entities/queue.entity';
 import { QueueService } from './queue.service';
 
 describe('QueueService', () => {
   let service: QueueService;
+  let queue  = null;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,5 +16,47 @@ describe('QueueService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should enqueue', () => {
+    queue = new Queue('test');
+    queue.enqueue("item1");
+    expect(queue.size()).toEqual(1);
+    queue.enqueue("item1");
+    expect(queue.size()).toEqual(2);
+    queue.enqueue("item1");
+    expect(queue.size()).toEqual(3);
+  });
+
+  it('should dequeue', () => {
+    queue = new Queue('test');
+    queue.enqueue("item1");
+    queue.enqueue("item2");
+    queue.enqueue("item3");
+    let res = queue.dequeue();
+    expect(queue.size()).toEqual(2);
+    expect(res).toEqual("item1");
+    res = queue.dequeue();
+    expect(queue.size()).toEqual(1);
+    expect(res).toEqual("item2");
+    res = queue.dequeue();
+    expect(queue.size()).toEqual(0);
+    expect(res).toEqual("item3");
+    queue.dequeue();
+    expect(queue.size()).toEqual(0);
+  });
+
+  it('should get snapshot', () => {
+    queue = new Queue('test');
+    queue.enqueue("item1");
+    let items = queue.getSnapshot();
+    expect(items.length).toEqual(1);
+    expect(items[0]).toEqual("item1");
+    queue.enqueue("item2");
+    queue.enqueue("item3");
+    items = queue.getSnapshot();
+    expect(items.length).toEqual(3);
+    expect(items[1]).toEqual("item2");
+    expect(items[2]).toEqual("item3");
   });
 });
